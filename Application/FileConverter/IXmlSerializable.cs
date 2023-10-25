d9db07496aa86abea71afe1eccd54fd743320ebc
simjust175 <bunim175@gmail.com>
2023-10-26 00:40:32 +0300

    added explanation

--- Application/FileConverter/IXmlSerializable.cs
+++
@@ -1,9 +0,0 @@
-﻿// <copyright file="IXmlSerializable.cs" company="AAllard">License: http://www.gnu.org/licenses/gpl.html GPL version 3.</copyright>
-
-namespace FileConverter
-{
-    public interface IXmlSerializable
-    {
-        void OnDeserializationComplete();
-    }
-}
